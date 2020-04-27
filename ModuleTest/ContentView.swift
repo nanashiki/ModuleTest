51519ae9e75d4c5f95566891d49036be0111b7d2
nanashiki <nanashiki.app@gmail.com>
2020-04-27 18:30:33 +0900

    Initial Commit

---
+++ ModuleTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ModuleTest
+//
+//  Created by nanashiki on 2020/04/27.
+//  Copyright © 2020 nanashiki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
